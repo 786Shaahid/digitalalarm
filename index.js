8a6a6c00fa7d0352d19ba89d2a3b8be16db77340
Shahid Raza <shahidraza02qwert@gmail.com>
2023-03-01 17:27:56 +0530

    final

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ let sec = document.querySelector('#sec').value;
 let display = document.querySelector('#displayTime');
 // let deleteTime = document.querySelector('#delete');
 // let alarmShow = document.querySelector('.alarmShow');
-let displayBox= document.querySelector('#btn');
+let displayBox = document.querySelector('#btn');
 
 let meridian = document.querySelector('#am_pm');
+// currentTime
 let currentTime = () => {
     let date = new Date();
     let hour = date.getHours();
@@ -23,94 +24,95 @@ let currentTime = () => {
     return cent.innerHTML = `${hour}:${min}:${sec} ${ampm}`;
 };
 function addZero(time) {
-        return time < 10 ? '0' + time : time;
+    return time < 10 ? '0' + time : time;
 };
 
+// set time
 setInterval(currentTime, 1000);
- 
+
 setTime = document.querySelector('#resetBtn').onclick = () => {
     min = document.querySelector('#min').value;
     sec = document.querySelector('#sec').value;
     hr = document.querySelector('#hour').value;
     meridian = document.querySelector('#am_pm').value;
-           
+
     if (hr != 00 || min != 00 || sec != 00) {
-        const alarm=`${hr}:${min}:${sec} ${meridian}`;
-               let alarmId='alarm1';
-     let noOfAlarms=localStorage.length;
-     if(noOfAlarms>0){
-        alarmId=    `alarm${noOfAlarms+1}`;
-     }
-         localStorage.setItem(alarmId, alarm);
-    //    console.log(localStorage.getItem(alarmId));
-       setAlarmTimeBox(alarmId,alarm);
-       startTimer(cent.innerHTML,alarm,alarmId)
-        } else {
+        const alarm = `${hr}:${min}:${sec} ${meridian}`;
+        let alarmId = 'alarm1';
+        let noOfAlarms = localStorage.length;
+        if (noOfAlarms > 0) {
+            alarmId = `alarm${noOfAlarms + 1}`;
+        }
+        localStorage.setItem(alarmId, alarm);
+        //    console.log(localStorage.getItem(alarmId));
+        setAlarmTimeBox(alarmId, alarm);
+        startTimer(cent.innerHTML, alarm, alarmId);
+    } else {
         alert('You are missing to fill either hrs,min or sec!, please fill left input ');
     }
- 
+
 };
 
-    function setAlarmTimeBox(id,alarm){
-        let deleteHTML=`delete-${id}`;
-              let alarmBox = 
-          `  <div id="display-box">
+function setAlarmTimeBox(id, alarm) {
+    let deleteHTML = `delete-${id}`;
+    let alarmBox =
+        `  <div id="display-box">
           <div class="alarmShow"  >
        <p id="displayTime">${alarm}</p>
        <p class="delete" id='${deleteHTML}'  title='Delete set alarm'>Delete</p>
      </div>
-     </div>`;       
-       displayBox.insertAdjacentHTML('afterend',alarmBox);
+     </div>`;
+    displayBox.insertAdjacentHTML('afterend', alarmBox);
 
-       deleteAlarmById(deleteHTML);
+    deleteAlarmById(deleteHTML);
     //    console.log(deleteHTML);
- }
- function deleteAlarmById(id) {
+}
+function deleteAlarmById(id) {
     let deleteTime = document.querySelector(`#${id}`);
     // console.log("delete function",  deleteTime );
-    deleteTime.addEventListener("click", function(){
+    deleteTime.addEventListener("click", function () {
         localStorage.removeItem(id);
-      this.parentElement.parentElement.remove();
-    //   this.parentElement.remove();
+        this.parentElement.parentElement.remove();
+        //   this.parentElement.remove();
     });
-} 
+}
 
-function startTimer(currentTime,alarm,id){
-    
-  let currTimeMeridian=currentTime.substring(currentTime.length-2,currentTime.length);
-  let alarmTimeMeridian=alarm.substring(alarm.length-2,alarm.length);
-  let currentArr=currentTime.split(':');
-  console.log(currentArr);
-  
-  let currentSeconds=currentArr[0]*3600+currentArr[1]*60+currentArr[2].split(' ')[0]*1+(currTimeMeridian==="PM" ? 43200 :0 );
-  let alarmArr=alarm.split(':');
-  let alarmSeconds=alarmArr[0]*3600+alarmArr[1]*60+alarmArr[2].split(' ')[0]*1+(alarmTimeMeridian==="PM" ? 43200 :0 );
-//    console.log(alarmSeconds);
-   let timerSeconds=
-   alarmSeconds-currentSeconds<0 ? 86400-Math.abs(alarmSeconds-currentSeconds):alarmSeconds-currentSeconds ;
+function startTimer(currentTime, alarm, id) {
+
+    let currTimeMeridian = currentTime.substring(currentTime.length - 2, currentTime.length);
+    let alarmTimeMeridian = alarm.substring(alarm.length - 2, alarm.length);
+    let currentArr = currentTime.split(':');
+    console.log(currentArr);
+
+    let currentSeconds = currentArr[0] * 3600 + currentArr[1] * 60 + currentArr[2].split(' ')[0] * 1 + (currTimeMeridian === "PM" ? 43200 : 0);
+    let alarmArr = alarm.split(':');
+    let alarmSeconds = alarmArr[0] * 3600 + alarmArr[1] * 60 + alarmArr[2].split(' ')[0] * 1 + (alarmTimeMeridian === "PM" ? 43200 : 0);
+    //    console.log(alarmSeconds);
+    let timerSeconds =
+        alarmSeconds - currentSeconds < 0 ? 86400 - Math.abs(alarmSeconds - currentSeconds) : alarmSeconds - currentSeconds;
     // console.log(timerSeconds);
- 
-    setTimeout(()=>{
+
+    setTimeout(() => {
         console.log(`${id}`);
         alert(`${id} is ringing now`);
-        let deleteHTML=`delete-${id}`;
-       let deleteTime=document.querySelector(`#${deleteHTML}`);
-    //    console.log(deleteTime);
-       localStorage.removeItem(id);
-       deleteTime.parentElement.parentElement.remove();    
+        let deleteHTML = `delete-${id}`;
+        let deleteTime = document.querySelector(`#${deleteHTML}`);
+        //    console.log(deleteTime);
+        localStorage.removeItem(id);
+        deleteTime.parentElement.parentElement.remove();
 
-        
 
-    },timerSeconds*1000)
+
+    }, timerSeconds * 1000)
 
 
 }
 
 
-  
 
 
-             
+
+
 
 
 
